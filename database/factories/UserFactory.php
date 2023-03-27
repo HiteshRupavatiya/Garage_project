584f82f4169621b9d43098624caa929604c866ae
hitesh <hiteshr@zignuts.com>
2023-03-27 19:30:02 +0530

    added factory definition for admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -18,11 +19,21 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'first_name'               => fake()->name(),
+            'last_name'                => fake()->lastName(),
+            'email'                    => fake()->unique()->email(),
+            'phone'                    => fake()->unique()->numberBetween(6000000000, 9999999999),
+            'email_verified_at'        => now(),
+            'password'                 => Hash::make(12345678),
+            'remember_token'           => Str::random(10),
+            'email_verification_token' => '',
+            'profile_picture'          => fake()->image(),
+            'type'                     => 'Admin',
+            'billable_name'            => fake()->name(),
+            'address1'                 => fake()->address(),
+            'address2'                 => fake()->address(),
+            'zip_code'                 => fake()->numberBetween(111111, 999999),
+            'city_id'                  => 1,
         ];
     }
 
