798fe014fcfa2927da5df250d66c4a5fed8d8d9d
hitesh <hiteshr@zignuts.com>
2023-03-28 19:32:36 +0530

    added welcome email for mechanic

---
+++ app/Mail/MechanicWelcomeEmail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MechanicWelcomeEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mechanic, $password;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($mechanic, $password)
+    {
+        $this->mechanic = $mechanic;
+        $this->password = $password;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address('hello@example.com'),
+            subject: 'Mechanic Welcome Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'Emails.mechanic_welcome_email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
